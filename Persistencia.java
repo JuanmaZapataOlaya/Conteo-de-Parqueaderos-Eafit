dde889b88319a13ac47f60e8c04e2a91027f0111
masalgaro <124186064+masalgaro@users.noreply.github.com>
2023-05-12 18:12:08 -0500

    Guardar más cosas y con mejor presentación
    Lo que dice el mensaje, el separador se hizo más largo porque ahora hay más información que guardar. Ahora se guarda el valor del boolean de discapacidad, y el texto en sí que se guarda fue alterado ligeramente para una mejor presentación

--- Persistencia.java
+++ Persistencia.java
@@ -4,8 +4,8 @@ import java.io.PrintWriter;
 import java.io.IOException;
 
 public class Persistencia{
-    public static void guardarUsuario(String stringUsuario, char placa){
-        File f = new File("Archivos del Usuario.txt"); //Especifica de 'f' será la abreviación del archivo de texto
+    public static void guardarUsuario(String stringUsuario, char placa, boolean discapacidad){
+        File f = new File("Archivos del Usuario.txt"); //Especifica que 'f' será la abreviación del archivo de texto
         FileWriter writer = null;
         PrintWriter pw = null;
         
@@ -22,8 +22,8 @@ public class Persistencia{
         try{
             writer = new FileWriter("Archivos del Usuario.txt",true);
             pw = new PrintWriter(writer);
-            pw.println("======================================="+"\n");
-            pw.println("ID usuario: " + stringUsuario + " Último dígito de placa: " + placa);
+            pw.println("======================================================================================================"+"\n");//Crea un separador para que no se confundan diferentes usuarios
+            pw.println("ID usuario: " + stringUsuario + " Último dígito de placa: " + placa + " ¿Posee discapacidad? true o false: " + discapacidad);
         }
         catch(IOException exception){
             System.err.println("Error abriendo el archivo guardado :c");
@@ -41,7 +41,7 @@ public class Persistencia{
     }
     
     public static void guardarCelda(String TicketeFinal){
-        File f = new File("Archivos del Usuario.txt"); //Especifica de 'f' será la abreviación del archivo de texto
+        File f = new File("Archivos del Usuario.txt");
         FileWriter writer = null;
         PrintWriter pw = null;
         
@@ -59,7 +59,42 @@ public class Persistencia{
             writer = new FileWriter("Archivos del Usuario.txt",true);
             pw = new PrintWriter(writer);
             pw.println("Celda: " + TicketeFinal + "\n");
-            pw.println("======================================="+"\n");
+            pw.println("======================================================================================================"+"\n");
+        }
+        catch(IOException exception){
+            System.err.println("Error abriendo el archivo guardado :c");
+        }
+        finally{
+            try{ //Esto intentará cerrar el archivo
+                if (null != writer){
+                    writer.close();
+                }
+            }
+            catch(Exception e2){
+                e2.printStackTrace();
+            }
+        }
+    }
+    
+    public static void guardarHora(String hora){
+        File f = new File("Archivos del Usuario.txt");
+        FileWriter writer = null;
+        PrintWriter pw = null;
+        
+        
+        //Este código se encarga de crear el archivo
+        if (!f.exists()){
+            try{
+                f.createNewFile();
+            }
+            catch(IOException exception){
+                System.err.println("Error creando el archivo :c");
+            }
+        }
+        try{
+            writer = new FileWriter("Archivos del Usuario.txt",true);
+            pw = new PrintWriter(writer);
+            pw.println("Hora asignada: "+ hora);
         }
         catch(IOException exception){
             System.err.println("Error abriendo el archivo guardado :c");
@@ -77,7 +112,7 @@ public class Persistencia{
     }
     
     public static void guardarSaldo(int saldo){
-        File f = new File("Archivos del Usuario.txt"); //Especifica de 'f' será la abreviación del archivo de texto
+        File f = new File("Archivos del Usuario.txt");
         FileWriter writer = null;
         PrintWriter pw = null;
         
