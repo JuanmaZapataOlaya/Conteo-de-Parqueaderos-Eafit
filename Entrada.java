5dd4c8e938a7e459555192552c20eb0dadb080d6
masalgaro <124186064+masalgaro@users.noreply.github.com>
2023-05-12 18:02:49 -0500

    Mejora al ingreso de información
    Ahora hay código que maneja el escenario hipotético de que un usuario ingrese información incompleta o inválida en el programa. Lo hace forzando valores por defecto en el programa para que siga en funcionamiento

--- Entrada.java
+++ Entrada.java
@@ -7,7 +7,10 @@ public class Entrada{
         Scanner Usuario = new Scanner(System.in);
         stringUsuario = Usuario.nextLine();
         System.out.println(stringUsuario);
-        
+        if (stringUsuario.length() == 0){
+            System.out.println("Ingresaste un documento inválido, automáticamente se usará el documento 0000000000");
+            stringUsuario = "0000000000";
+        }
         return (stringUsuario);
     }
     
@@ -15,14 +18,21 @@ public class Entrada{
         System.out.println("Ingrese la placa del vehiculo");
         Scanner Placa = new Scanner(System.in);
         placaString = Placa.nextLine();
-        char lastPlaca = placaString.charAt(5);
+        char lastPlaca = '0';
+        if (placaString.length() < 5){ //Arregla que pongan una placa no valida
+            System.out.println("La placa ingresada no es válida, se continuará asignando automáticamente la placa AAA000");
+        }
+        else{
+            lastPlaca = placaString.charAt(5);
+        }
+        
         System.out.println(lastPlaca);
         
         return (lastPlaca);
     }
     
     public static String entradaDia(){
-        System.out.println("Ingrese el dia de la semana: ");
+        System.out.println("Ingrese el día de la semana: ");
         Scanner SemanaDia = new Scanner(System.in);
         String Dia = SemanaDia.nextLine();
         
@@ -40,5 +50,4 @@ public class Entrada{
         }
         return false;
     }
-    
 }
